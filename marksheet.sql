70238c3938f53825140fd09f3ff9ea57d32ede01
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2023-10-02 19:51:45 +0600

    Update marksheet.sql

--- marksheet.sql
+++ marksheet.sql
@@ -8,7 +8,7 @@ Name VARCHAR(50),
 GPA FLOAT NOT NULL
 );
 
---Method 1
+-- Method 1
 INSERT INTO Students
 (ID, Name, GPA)
 VALUES
@@ -20,12 +20,12 @@ VALUES
 (6, "Minhaz Aritro", 4.58);
 
 -- Method 2 (Short)
-INSERT INTO Students VALUES (1, "Fabian Roy", 4.67);
-INSERT INTO Students VALUES (2, "Mujtabir Nayeef", 5.0);
-INSERT INTO Students VALUES (3, "Mahir Riju", 4.58);
-INSERT INTO Students VALUES (4, "Nasif Fuad", 4.58);
-INSERT INTO Students VALUES (5, "Aishorzo Hira", 5.0);
-INSERT INTO Students VALUES (6, "Minhaz Aritro", 4.58);
+INSERT INTO Students VALUES (7, "Fabian Roy", 4.67);
+INSERT INTO Students VALUES (8, "Mujtabir Nayeef", 5.0);
+INSERT INTO Students VALUES (9, "Mahir Riju", 4.58);
+INSERT INTO Students VALUES (10, "Nasif Fuad", 4.58);
+INSERT INTO Students VALUES (11, "Aishorzo Hira", 5.0);
+INSERT INTO Students VALUES (12, "Minhaz Aritro", 4.58);
 
 SELECT * FROM Students;
 SHOW DATABASES;
