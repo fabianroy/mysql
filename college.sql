d3d4f78d2d2b661cf348f84713d15a8c15f8d7f9
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2023-10-03 19:43:43 +0600

    Create college.sql

---
+++ college.sql
@@ -0,0 +1,19 @@
+Roll INT NOT NULL,
+Grade VARCHAR(30),
+City VARCHAR(30)
+);
+
+INSERT INTO students
+(ID, Name, Roll, Grade, City)
+VALUES
+(101, "Fabian", 775, "A", "Dhaka"),
+(102, "Nasif", 714, "A", "Dhaka"),
+(103, "Mahir", 756, "A+", "Dhaka"),
+(104, "Mujtabir", 780, "A-", "Dhaka");
+
+SELECT Name, Grade FROM students;
+SELECT DISTINCT Grade FROM students;
+SELECT * FROM students WHERE Grade = "A";
+SELECT * FROM students WHERE Roll > 750;
+SELECT * FROM students WHERE Grade = "A" AND ROll > 750;
+SELECT * FROM students;
