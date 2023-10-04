74e3f788327471b7e03f2089195082c9374d89fa
Fabian Roy <111523461+fabianroy@users.noreply.github.com>
2023-10-04 19:57:12 +0600

    Update college.sql

--- college.sql
+++ college.sql
@@ -22,4 +22,23 @@ SELECT DISTINCT Grade FROM students;
 SELECT * FROM students WHERE Grade = "A";
 SELECT * FROM students WHERE Roll > 750;
 SELECT * FROM students WHERE Grade = "A" AND ROll > 750;
+SELECT * FROM students WHERE Grade = "A" OR Roll > 750;
+SELECT * FROM students WHERE Roll BETWEEN 750 AND 775;
+SELECT * FROM students WHERE City IN ("Dhaka");
+SELECT * FROM students WHERE Grade NOT IN ("A");
+SELECT * FROM students LIMIT 2;
+SELECT * FROM students ORDER BY Name ASC;
+SELECT * FROM students ORDER BY Name DESC;
 SELECT * FROM students;
+
+/* Aggregate Functions */
+
+SELECT MAX(Roll) FROM students;
+SELECT AVG(Roll) FROM students;
+SELECT MIN(Roll) FROM students;
+SELECT COUNT(Roll) FROM students;
+SELECT SUM(Roll) FROM students;
+
+/* Group By Clause */
+
+SELECT Grade, COUNT(Roll) FROM students GROUP BY Grade;
